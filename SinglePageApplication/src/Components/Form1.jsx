716a032b8d370af330f5c3ff6e533361da95dc05
Joao Wictor <119543998+WictorPereira@users.noreply.github.com>
2024-06-19 20:05:38 -0300

    btn delete +Correções
    Logica do btn delete  funcionando. correções nos atributos name e ultima modificação, para extension e count

--- SinglePageApplication/src/Components/Form1.jsx
+++ SinglePageApplication/src/Components/Form1.jsx
@@ -1,80 +1,90 @@
-import './Form1.css'
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./Form1.css";
 
-
-function Form1 () {
+function Form1() {
   const [selectedFile, setSelectedFile] = useState(null);
+  const [charCount, setCharCount] = useState(0);
 
   const handleFileChange = (e) => {
-  const file = e.target.files[0]
+    const file = e.target.files[0];
     setSelectedFile(file);
-  }
 
-  //Evento do botao envair o dados do arquivo para o db.json
-  function btnImport (event) {
-    event.preventDefault()
+    if (file) {
+      // Ler o conteúdo do arquivo como texto para contar os caracteres
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        const content = e.target.result;
+        const count = content.length;
+        setCharCount(count);
+      };
+      reader.readAsText(file);
+    } else {
+      setCharCount(0);
+    }
+  };
+
+  // Evento do botão para enviar os dados do arquivo para o db.json
+  function btnImport(event) {
+    event.preventDefault();
 
     if (!selectedFile) {
-      console.log("Nenhum arquivo selecionado.")
+      console.log("Nenhum arquivo selecionado.");
       return;
     }
 
     const fileDetails = {
-      name: selectedFile.name,
-      lastModified: selectedFile.lastModified,
-      size: selectedFile.size,
-    }
-    
-    fetch("http://localhost:5000/arquivos",{
-      method: "POST",
-      headers:{
-        'Content-Type': 'application/json',
-      },
-      body:(JSON.stringify(fileDetails))
-    }
-  )
-  .then((resp) => {
-    if(!resp.ok) {
-      throw new Error('Erro ao enviar os dados.')
-    }
-    return resp.json();
-  })
-  .then((data)=>{ console.log("Dados enviados com sucesso:", data)
-  })
-  .catch((err)=> console.log("Erro ao enviar os dados:", err))
-  
-}
+      extension: selectedFile.name.split(".").pop(),
+      count: charCount,
+      filesize: selectedFile.size,
+    };
 
-  
-  return(
-    <div className='div-forms'>
+    fetch("http://localhost:5000/arquivos", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(fileDetails),
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("Erro ao enviar os dados.");
+        }
+        return resp.json();
+      })
+      .then((data) => {
+        console.log("Dados enviados com sucesso:", data);
+      })
+      .catch((err) => console.log("Erro ao enviar os dados:", err));
+  }
+
+  return (
+    <div className="div-forms">
       <form className="form">
-          <h1 className='h1-title'>Add by importing JSON file</h1>
-          <h4 className='h4-choose'>Choose a file</h4>
-          <input 
-            className='file-input' 
-            type="file" 
-            accept='.json'
-            onChange={handleFileChange}
-          />
-          {selectedFile && (
-          <div>         
-            <p className='p-results'>Nome do arquivo: {selectedFile.name} </p>
-            <p className='p-results'>Couts do arquivo: {selectedFile.lastModified}</p>
-            <p className='p-results'>Tamanho do arquivo: {selectedFile.size} bytes </p>  
-               
+        <h1 className="h1-title">Add by importing JSON file</h1>
+        <h4 className="h4-choose">Choose a file</h4>
+        <input
+          className="file-input"
+          type="file"
+          accept=".json"
+          onChange={handleFileChange}
+        />
+        {selectedFile && (
+          <div className="div-p-results">
+            <p className="p-results">
+              Extensão do arquivo: {selectedFile.name}
+            </p>
+            <p className="p-results">Count: {charCount} caracteres</p>
+            <p className="p-results">filesize: {selectedFile.size} bytes
+            </p>
           </div>
-          
         )}
 
-         <button className='btn-import' onClick={btnImport}>Import</button>   
-         
-         
-             
-    </form>
+        <button className="btn-import" onClick={btnImport}>
+          Import
+        </button>
+      </form>
     </div>
-  )
+  );
 }
 
-
-export default Form1
+export default Form1;
--- SinglePageApplication/src/Components/Results.jsx
+++ SinglePageApplication/src/Components/Results.jsx
@@ -2,10 +2,27 @@ import "./Table.css";
 
 import React, { useState, useEffect } from "react";
 
-
 function Results() {
+  function removeFiles(id) {
+    fetch(`http://localhost:5000/arquivos/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => {
+        if (!resp.ok) {
+          throw new Error("Erro ao deletar o arquivo");
+        }
+
+        setFiles(files.filter((file) => file.id !== id));
+      })
+      .catch((err) => console.log(err));
+  }
+
   const [files, setFiles] = useState([]);
 
+  //buscando arquivos db.json
   useEffect(() => {
     fetch("http://localhost:5000/arquivos")
       .then((response) => {
@@ -14,47 +31,39 @@ function Results() {
         }
         return response.json();
       })
-      .then((data) => { 
-        setFiles(data);        
+      .then((data) => {
+        setFiles(data);
       })
       .catch((error) => {
         console.error("Erro ao buscar arquivos", error);
       });
-  }, [files]);// Executar apenas uma vez após a montagem do componente
+  }, [files]); // Executar apenas uma vez após a montagem do componente
 
   // Verifica se files ainda está vazio ou indefined
   if (!files || files.length === 0) {
     return (
-        <p className="p-table">
-          Add new data by importing it from a file or filling in the fields above!
-        </p>
-    )
-  }
-    return (
-      
-      <ul className="ul-results">
-        {files.map((file, index) => (
-          <li key={index} 
-              className="li-results"> 
-              <span>{file.name}</span> 
-              <span>{file.lastModified}</span>
-              <span>{file.size}</span>
-              <div className="div-btns-results">
-                <button className='btn-edit'>Edit</button> 
-                <button className='btn-delete'>Delete</button> 
-              </div>
-              </li>
-        ))}
-      </ul>
+      <p className="p-table">
+        Add new data by importing it from a file or filling in the fields above!
+      </p>
     );
   }
+  return (
+    <ul className="ul-results">
+      {files.map((file, index) => (
+        <li key={index} className="li-results">
+          <span>{file.extension}</span>
+          <span>{file.count}</span>
+          <span>{file.filesize}</span>
+          <div className="div-btns-results">
+            <button className="btn-edit">Edit</button>
+            <button className="btn-delete" onClick={() => removeFiles(file.id)}>
+              Delete
+            </button>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 export default Results;
-
-/*<li className='li-results'>Nome do arquivo: </li>
-            <li className='li-results'>Última modificação:</li>
-            <li className='li-results'>Tamanho do arquivo:</li>
-            <li className='li-results'>
-            <button className='btn-edit'>Edit</button> 
-            <button className='btn-delete'>Delete</button> 
-            */
